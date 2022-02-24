875041a8ab5de21f0a12c82b6496c7b9d0b5a6c0
leo-yuen-sanuker <leo.yuen@sanuker.com>
2022-02-24 07:30:21 +0000

    rfadvs

--- web.js
+++ web.js
@@ -10,15 +10,95 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   let reqBody = req.body
-  console.log("req.body: ", JSON.stringify(reqBody, null, 2).replaceAll("\n", "\\n"));
+  console.log("req.body: ", JSON.stringify(reqBody, null, 2).replace("\n", "\\n"));
 
 //   let myself = "https://nodejs-bot-25cbybmeaa-uc.a.run.app"
   let myself = reqBody._links.self.href
-  let myselfStatus = Object.entries(reqBody.arena.state).find(e=>(e[0] === myself))
-  console.log("myselfStatus: ", myselfStatus)
+  let myselfState = Object.entries(reqBody.arena.state).find(e=>(e[0] === myself))
+  console.log("myselfState: ", myselfState)
 
+  let faceLeft = 0
+  let faceRight = 0
+  let faceUp = 0
+  let faceDown = 0
+  let blockedLeft = 0
+  let blockedRight = 0
+  let blockedUp = 0
+  let blockedDown = 0
+  let allState = Object.entries(reqBody.arena.state).map( e => {
+    if (e[0] === myself) {
+      return ({...e[1], "myself": true})
+    }
+    let result = {
+      ...e[1],
+      "xDiff": myselfState.x - e[1].x,
+      "yDiff": myselfState.y - e[1].y,
+    }
+    if (result.xDiff === 0) {
+      if (!blockedUp) {
+        if (result.yDiff === 1) {
+          blockedUp = 1
+        }
+      }
+      if (!blockedDown) {
+        if (result.yDiff === -1) {
+          blockedDown = 1
+        }
+      }
+      if (!faceUp) {
+        if ((result.yDiff > 0) && (result.direction === "S")) {
+          faceDown = 1
+        }
+      }
+      if (!faceDown) {
+        if ((result.yDiff < 0) && (result.direction === "N")) {
+          faceUp = 1
+        }
+      }
+    }
+    if (result.yDiff === 0) {
+      if (!blockedRight) {
+        if (result.yDiff === 1) {
+          blockedRight = 1
+        }
+      }
+      if (!blockedLeft) {
+        if (result.yDiff === -1) {
+          blockedLeft = 1
+        }
+      }
+      if (!faceLeft) {
+        if ((result.xDiff < 0) && (result.direction === "W")) {
+          faceLeft = 1
+        }
+      }
+      if (!faceLeft) {
+        if ((result.xDiff > 0) && (result.direction === "E")) {
+          faceRight = 1
+        }
+      }
+    }
+    return result
+  })
+
+  let runAway = false
+  if ((faceLeft+faceRight+faceUp+faceDown) > 2) {
+    runAway = true
+  }
+  let blocked = false
+  if (runAway) {
+    let cantF = (blockedLeft && (myselfState.direction === "W")) ||(blockedRight && (myselfState.direction === "E")) ||(blockedUp && (myselfState.direction === "N")) ||(blockedDown && (myselfState.direction === "S"))
+    if (cantF) {
+      res.send('L')
+    } else {
+      res.send('F')
+    }
+  } else {
+    res.send('T')
+  }
   const moves = ['F', 'T', 'L', 'R'];
-  // t23r532egwr3hrt
+
+
   // res.send(moves[Math.floor(Math.random() * moves.length)]);
   res.send('L');
 });
